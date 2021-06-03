b7cfd507785d2fd7308289ebf4b50afd4ec30cb3
Helena Ribera <hribera.crm@gmail.com>
2021-06-03 15:40:23 -0500

    adding example from circuit data.

---
+++ Example_LorenzCircuit/Read_Files.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# Read the input text files.
+# There's three of them...
+#    1) File1.txt includes information about the number of state variables, 
+#       parameters, measured state variables, time step and total number of time points;
+#       the variable and parameter names; the equations of the system at hand; 
+#       the bounds for both state variables and parameters search.
+#    2) File2.txt includes information about the measured state variables, 
+#       the files in which the data can be found and the form the measurement part of the action takes.
+#    3) File3.txt inlcudes information about parameters needed to do the variational annealing
+#       as well as the search in lambda.
+
+Input1 = np.genfromtxt('File1.txt', comments = '#', dtype = 'str')
+Input2 = np.genfromtxt('File2.txt', comments = '#', dtype = 'str')
+Input3 = np.genfromtxt('File3.txt', comments = '#', dtype = 'str')
+
+# We make sure that parameters that will be used as limits in for loops are integers
+# and that the time step is a float.
+num_vars = int(Input1[0].split(",")[0])
+#num_params = int(Input1[0].split(",")[1])
+num_meas = int(Input1[0].split(",")[2])
+dt = float(Input1[0].split(",")[3])
+num_tpoints = int(Input1[0].split(",")[4])
+
+# This is the total number of variables that IPOPT will consider.
+#num_total = num_vars*num_tpoints + num_params
+
+# Here we save all the data from the different measured state variables.
+data = np.zeros((num_tpoints,num_meas))
+
+for i in range(num_meas):
+#    filename = "%s" % Input2[num_meas+1+i]
+    filename = "%s" % Input2[num_meas+i]
+    data[:,i] = np.loadtxt(filename)
+
+# We transform it from a matrix of 'num_tpoints' by 'num_meas' to just a vector.  
+dataT = np.reshape(data,num_tpoints*num_meas)
+    
+# 
+alpha = float(Input3[0])
+
+# Number of annealing steps.
+beta_max = int(Input3[1])
+
+# R_{f,0}.
+Rf0 = Input3[2:2+num_vars].astype(np.float)
+
+# First lambda in our sweep.
+lambd_0 = float(Input3[2+num_vars])
+
+# Maximum lambda in our sweep.
+lambd_max = float(Input3[2+num_vars+1])
---
+++ Example_LorenzCircuit/WriteStrings.py
@@ -0,0 +1,90 @@
+from header import *
+import time
+import sys
+from sympy import symbols
+import pickle
+import numpy as np
+
+Input1 = np.genfromtxt('File1.txt', comments = '#', dtype = 'str')
+
+# We make sure that parameters that will be used as limits in for loops are integers
+# and that the time step is a float.
+num_vars = int(Input1[0].split(",")[0])
+num_params = int(Input1[0].split(",")[1])
+num_meas = int(Input1[0].split(",")[2])
+dt = float(Input1[0].split(",")[3])
+num_tpoints = int(Input1[0].split(",")[4])
+
+# This is the total number of variables that IPOPT will consider.
+num_total = num_vars*num_tpoints + num_params
+
+file_ObjJacHess = open('ObjJacHess.obj', 'r') 
+
+ObjFunk_Meas_eval = pickle.load(file_ObjJacHess)
+ObjFunk_Model_eval = pickle.load(file_ObjJacHess)
+Jacobian_Meas = pickle.load(file_ObjJacHess)
+Jacobian_Model = pickle.load(file_ObjJacHess)
+Hessian_Meas = pickle.load(file_ObjJacHess)
+Hessian_Model = pickle.load(file_ObjJacHess)
+
+string_Jac_Meas = ""
+for i in range(3*num_vars+num_params):
+    string_Jac_Meas += "        str_JacFunk_Meas[0,%d] = %s\n" %(i,Jacobian_Meas[0,i])
+
+string_Jac_Model = ""
+for i in range(3*num_vars+num_params):
+    string_Jac_Model += "        str_JacFunk_Model[0,%d] = %s\n" %(i,Jacobian_Model[0,i])     
+
+string_Jac_Meas_LessIndent = ""
+for i in range(3*num_vars+num_params):
+    string_Jac_Meas_LessIndent += "    str_JacFunk_Meas[0,%d] = %s\n" %(i,Jacobian_Meas[0,i])
+
+###############################################################################
+
+string_Hess_Meas = ""
+for j in range(3*num_vars+num_params):
+    for i in range(3*num_vars+num_params):
+        string_Hess_Meas += "            str_HessFunk_Meas[%d,%d] = %s\n" %(j,i,Hessian_Meas[j,i])  
+string_Hess_Meas += "\n"
+        
+string_Hess_Model = ""
+for j in range(3*num_vars+num_params):
+    for i in range(3*num_vars+num_params):
+        string_Hess_Model += "            str_HessFunk_Model[%d,%d] = %s\n" %(j,i,Hessian_Model[j,i])          
+            
+string_Hess_Meas_LessIndent = ""
+for j in range(3*num_vars+num_params):
+    for i in range(3*num_vars+num_params):
+        string_Hess_Meas_LessIndent += "        str_HessFunk_Meas[%d,%d] = %s\n" %(j,i,Hessian_Meas[j,i])  
+          
+
+###############################################################################
+
+# Open and read the template file "OneLoopInC_Blank.pyx"
+file1 = open("OneLoopInC_Blank.pyx","r") 
+
+# Each line in file gets saved in internal.
+internal = file1.readlines()
+file1.close()
+
+# We add to the corresponding lines the Model and Measurement strings.
+internal[73] = "        Objective += %s\n" %ObjFunk_Meas_eval
+internal[74] = "        Objective += %s\n" %ObjFunk_Model_eval
+internal[94] = "    Objective += %s\n" %ObjFunk_Meas_eval
+
+internal[137] = "%s" %string_Jac_Meas
+internal[139] = "%s" %string_Jac_Model        
+internal[168] = "%s" %string_Jac_Meas_LessIndent
+
+internal[233] = "%s" %string_Hess_Meas
+internal[235] = "%s" %string_Hess_Model        
+internal[274] = "%s" %string_Hess_Meas_LessIndent
+       
+newinternal = ''.join(internal)
+
+# Create the new file which has the right strings for the Model and Measurement parts.
+file2= open("OneLoopInC.pyx","w+")
+file2.write(newinternal)
+file2.close()
+
+print("\n\tThe strings were successfully written in the Cython file.\n")
